d00d53b366b1d880885fce0537839aeef97666d3
Nicolas Bonamy <nicolas@bonamy.fr>
2023-07-01 15:20:55 -0500

    mutex for sending ir

--- Makefile
+++ Makefile
@@ -3,3 +3,8 @@ install:
 	sudo systemctl enable pylirc-rc-1237.service
 	sudo systemctl start pylirc-rc-1237.service
 	
+status:
+	sudo service pylirc-rc-1237 status
+
+restart:
+	sudo service pylirc-rc-1237 restart
--- src/lirc_handler.py
+++ src/lirc_handler.py
@@ -1,17 +1,20 @@
 import lirc
+from threading import Lock
 
 class LircHandler:
 
   def __init__(self, config):
     self._config = config
     self._client = lirc.Client()
+    self._mutex = Lock()
 
   def handle(self, command):
-    try:
-      print(f'[LIRC] Sending command {command}')
-      self._client.send_once(self._config.get('lirc.device', 'Denon_RC-1237_raw'), command)
-      return True
-    except lirc.exceptions.LircdCommandFailureError as error:
-      print(f'[LIRC] Error while executing LIRC command {command}')
-      print(f'[LIRC] {error})') 
-      return False
+    with self._mutex:
+      try:
+        print(f'[LIRC] Sending command {command}')
+        self._client.send_once(self._config.get('lirc.device', 'Denon_RC-1237_raw'), command)
+        return True
+      except lirc.exceptions.LircdCommandFailureError as error:
+        print(f'[LIRC] Error while executing LIRC command {command}')
+        print(f'[LIRC] {error})') 
+        return False
